c5fc56ce43cdda44f1ba731296c71aacbd8d4ee3
Vinh Nguyen <vinh_nguyen1211@yahoo.com.vn>
2018-04-17 00:55:30 +0700

    update interface

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -2,8 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import 'antd/dist/antd.css';
-import { Form, Icon, Input, Button } from 'antd';
+import { Form, Icon, Input, Button, Table } from 'antd';
 const FormItem = Form.Item;
+// import Skycons from './dist/ReactSkycons'
 
 function hasErrors(fieldsError) {
   return Object.keys(fieldsError).some(field => fieldsError[field]);
@@ -16,7 +17,7 @@ class App extends Component {
       location_request: null,
       address: null,
       current_temp: 0,
-      next_weather_list: [],
+      next_weather_list: null,
     };
   }
 
@@ -26,9 +27,24 @@ class App extends Component {
 
   handleSubmit = (e) => {
     e.preventDefault();
+    
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        
+        fetch(`/weather?location=${values.location}`,{
+          method: 'GET',
+        })
+          .then(res => res.json())
+          .then(json => {
+            this.setState({
+              address: json.address,
+              current_temp: json.temp,
+              next_weather_list: json.nextWeather,
+            }, () => {
+              console.log(this.state.next_weather_list);
+            })
+          })
+          .catch(e => console.log(e));
       }
     });
   }
@@ -50,52 +66,81 @@ class App extends Component {
       .catch(e => console.log(e));
   }
 
-  handleChangeInput = (e) => {
-    this.setState({
-      location_request: e.target.value
-    })
-  }
-
   render() {
     const { getFieldDecorator, getFieldsError, getFieldError, isFieldTouched } = this.props.form;
 
     // Only show error after a field is touched.
-    const userNameError = isFieldTouched('userName') && getFieldError('userName');
-    const passwordError = isFieldTouched('password') && getFieldError('password');
+    const locationError = isFieldTouched('location') && getFieldError('location');
+    const btn_style = {
+      textAlign: 'center',
+      border: '2px solid #4286f4'
+    }
+    const columns = [{
+      title: 'Time Approximately',
+      dataIndex: 'time',
+    }, {
+      title: 'Temperature (feels like)',
+      dataIndex: 'apparentTemperature',
+    },{
+      title: 'Description',
+      dataIndex: 'summary',
+    },{
+      title: 'Icon',
+      dataIndex: 'icon',
+    }]
+    const data = this.state.next_weather_list && this.state.next_weather_list.map( (wh, index) => {
+      return({
+        key: index,
+        ...wh,
+        time: new Date(Number(wh.time*1000)).toLocaleString(),
+      })
+    });
     return (
         <div>
-          <Form layout="inline" onSubmit={this.handleSubmit}>
-            <FormItem
-              validateStatus={userNameError ? 'error' : ''}
-              help={userNameError || ''}
+          <h1>Weather App</h1>
+          <div className="form-location">
+          <Form onSubmit={this.handleSubmit}>
+            <label>Location</label>
+            <FormItem 
+              validateStatus={locationError ? 'error' : ''}
+              help={locationError || ''}
             >
-              {getFieldDecorator('userName', {
-                rules: [{ required: true, message: 'Please input your username!' }],
+              {getFieldDecorator('location', {
+                rules: [{ required: true, message: 'Please pick up a location!' }],
               })(
-                <Input prefix={<Icon type="place" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
+                <Input size="large" prefix={<Icon type="environment-o" />} placeholder="ZIP Code; City; Province; Address..." />
               )}
             </FormItem>
-            <FormItem>
+            <FormItem style={{textAlign: "center"}}>
               <Button
                 type="primary"
                 htmlType="submit"
+                icon="search"
+                size="large"
+                style={btn_style}
                 disabled={hasErrors(getFieldsError())}
               >
-                Log in
+                Search
               </Button>
             </FormItem>
+            {this.state.address ? <FormItem className="text-result">{this.state.address}</FormItem> : null}
+            {this.state.current_temp ? <FormItem>Current temp (feels like): {this.state.current_temp}</FormItem> : null}
           </Form>
-          
-          <form onSubmit={this.handleClick}>
+          </div>
+          {/* <form onSubmit={this.handleClick}>
               
               <div className="form-group">
                   <label>Location</label>
                   <input type="text" className="form-control" placeholder="ZIP Code; City; Province; Address..." onChange={this.handleChangeInput} />
               </div>
-          
-              <Button icon="search">Submit</Button>
-              
-          </form>
+          </form> */}
+          <div>
+            
+            {this.state.next_weather_list ? <Table className="weather-table" columns={columns} dataSource={data} size="middle" bordered /> : null}
+          </div>
+          <div>
+          {/* <Skycons color='white' icon='WIND' /> */}
+          </div>
         </div>
       );
   }
